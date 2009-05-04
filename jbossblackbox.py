51f8cb0148e62b5a6e77775115f4b58fe5d919de
Michael Neale <michael.neale@gmail.com>
2009-05-05 08:40:10 +1000

    more noodling

--- jbossblackbox.py
+++ jbossblackbox.py
@@ -5,48 +5,56 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-class Greeting(db.Model):
-  author = db.UserProperty()
-  content = db.StringProperty(multiline=True)
+
+
+class Report(db.Model):
+  project = db.StringProperty(multiline=False)
+  application = db.StringProperty(multiline=False)
+  version = db.StringProperty(multiline=False)
   date = db.DateTimeProperty(auto_now_add=True)
+  content = db.StringProperty(multiline=True)
+
 
 class MainPage(webapp.RequestHandler):
   def get(self):
     self.response.out.write('<html><body>')
 
-    greetings = db.GqlQuery("SELECT * FROM Greeting ORDER BY date DESC LIMIT 10")
+    reports = db.GqlQuery("SELECT * FROM Report ORDER BY date DESC LIMIT 10")
 
-    for greeting in greetings:
-      if greeting.author:
-        self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
-      else:
-        self.response.out.write('An anonymous person wrote:')
-      self.response.out.write('<blockquote>%s</blockquote>' %
-                              cgi.escape(greeting.content))
+    for report in reports:
+        self.response.out.write('Project : <b>%s</b><br/>' % report.project)
+        self.response.out.write('Date : <b>%s</b><br/>' % report.date)
+        self.response.out.write('Report : <blockquote>%s</blockquote><br/>' %
+                              cgi.escape(report.content))
+        self.response.out.write("Path : <i>%s</i><br/>" % self.request.path)
 
     # Write the submission form and the footer of the page
     self.response.out.write("""
-          <form action="/sign" method="post">
+          <form action="/manual" method="post">
+            <div><input type="text" name="project" /></div>
             <div><textarea name="content" rows="3" cols="60"></textarea></div>
-            <div><input type="submit" value="Sign Guestbook"></div>
+            <div><input type="submit" value="Create report"></div>
           </form>
         </body>
       </html>""")
 
-class Guestbook(webapp.RequestHandler):
+class ManualCreate(webapp.RequestHandler):
   def post(self):
-    greeting = Greeting()
+    report = Report()
 
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
-
-    greeting.content = self.request.get('content')
-    greeting.put()
+    report.content = self.request.get('content')
+    report.project = self.request.get('project')
+    report.put()
     self.redirect('/')
 
+class AgentCreate(webapp.RequestHandler): 
+  def post(self):
+
+
+
 application = webapp.WSGIApplication(
-                                     [('/', MainPage),
-                                      ('/sign', Guestbook)],
+                                     [('/list', MainPage),
+                                      ('/manual', ManualCreate)],
                                      debug=True)
 
 def main():
