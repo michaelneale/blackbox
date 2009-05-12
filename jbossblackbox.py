3b7d82430c8968adda49315c89b760953e4f1919
Michael Neale <michael.neale@gmail.com>
2009-05-12 18:37:04 +1000

    added in usage reporting

--- jbossblackbox.py
+++ jbossblackbox.py
@@ -60,7 +60,7 @@ class CreateUsageReport(webapp.RequestHandler):
 class ReportViewerCsv(webapp.RequestHandler): 
   def get(self): 
     checkAuth(self)
-    reports = db.GqlQuery("SELECT * FROM ErrorReport ORDER BY date DESC LIMIT 10")
+    reports = makeQuery("ErrorReport", self.request.get('project'), self.request.get('module'), self.request.get('version'), self.request.get('from'), self.request.get('to'))              
     self.response.headers['Content-Type'] = 'text/plain'
     self.response.out.write("Project,Module,Version,Date,Contact Name,Contact Email,Report\n")
     for report in reports:
@@ -76,8 +76,7 @@ class ReportViewerCsv(webapp.RequestHandler):
 class ReportViewerXML(webapp.RequestHandler): 
   def get(self): 
     checkAuth(self)
-    reports = makeQuery(self.request.get('project'), self.request.get('module'), self.request.get('version'), self.request.get('from'), self.request.get('to'))
-              
+    reports = makeQuery("ErrorReport", self.request.get('project'), self.request.get('module'), self.request.get('version'), self.request.get('from'), self.request.get('to'))              
     self.response.headers['Content-Type'] = 'text/xml'
     self.response.out.write("<error-reports>")
     for report in reports:
@@ -92,9 +91,28 @@ class ReportViewerXML(webapp.RequestHandler):
         self.response.out.write('</report>')
     self.response.out.write('</error-reports>')
 
+class UsageViewerXML(webapp.RequestHandler): 
+  def get(self): 
+    checkAuth(self)
+    reports = makeQuery("UsageReport", self.request.get('project'), self.request.get('module'), self.request.get('version'), self.request.get('from'), self.request.get('to'))              
+    self.response.headers['Content-Type'] = 'text/xml'
+    self.response.out.write("<usage-reports>")
+    for report in reports:
+        self.response.out.write('<report>')
+        self.response.out.write('<project>%s</project>' % report.project)
+        self.response.out.write('<module>%s</module>' % report.module)
+        self.response.out.write('<version>%s</version>' % report.version)
+        self.response.out.write('<date>%s</date>' % report.date)
+        self.response.out.write('<ip>%s</ip>' % report.ip)
+        self.response.out.write('<report><![CDATA[%s]]></report>' % report.content)
+        self.response.out.write('</report>')
+    self.response.out.write('</usage-reports>')
 
-def makeQuery(project, module, version, from_date, to_date):
-  q = 'where project = :project'
+
+def makeQuery(entity, project, module, version, from_date, to_date):
+  if not project:
+    return db.GqlQuery("select * from " + entity + " order by date DESC LIMIT 100")
+  q = 'select * from ' + entity + ' where project = :project'
   if (from_date):
     q = q + " and date <= DATETIME(:to_date)"
   if (to_date):
@@ -103,7 +121,7 @@ def makeQuery(project, module, version, from_date, to_date):
     q = q + " and module = :module"
   if (version):
     q = q + " and version = :version"
-  return  ErrorReport.gql(q, project=project, version=version, module=module, from_date=from_date, to_date=to_date)
+  return  db.GqlQuery(q, project=project, version=version, module=module, from_date=from_date, to_date=to_date)
 
 
 #this is for debugging really...
@@ -111,17 +129,6 @@ class ErrorMessagesAdmin(webapp.RequestHandler):
   def get(self):
     checkAuth(self)
     self.response.out.write('<html><body>')
-
-    reports = db.GqlQuery("SELECT * FROM ErrorReport ORDER BY date DESC LIMIT 10")
-
-    for report in reports:
-        self.response.out.write('Project : <b>%s</b><br/>' % report.project)
-        self.response.out.write('Module : <b>%s</b><br/>' % report.module)
-        self.response.out.write('Version : <b>%s</b><br/>' % report.version)
-        self.response.out.write('Date : <b>%s</b><br/>' % report.date)
-        self.response.out.write('Contact Name : <b>%s</b><br/>' % report.contact_name)
-        self.response.out.write('Contact Email : <b>%s</b><br/>' % report.contact_email)
-        self.response.out.write('Report : <blockquote>%s</blockquote><br/>' % cgi.escape(report.content))
 
     # Write the submission form and the footer of the page
     self.response.out.write("""
@@ -136,6 +143,24 @@ class ErrorMessagesAdmin(webapp.RequestHandler):
           </form>
         </body>
       </html>""")
+
+class UsageAdmin(webapp.RequestHandler):
+  def get(self):
+    checkAuth(self)
+    self.response.out.write('<html><body>')
+
+    # Write the submission form and the footer of the page
+    self.response.out.write("""
+          <form action="/create_usage" method="post">
+            <div>Project: <input type="text" name="project" /></div>
+            <div>Module: <input type="text" name="module" /></div>
+            <div>Version: <input type="text" name="version" /></div>
+            <div>Report: <textarea name="content" rows="3" cols="60"></textarea></div>
+            <div><input type="submit" value="Create report"></div>
+          </form>
+        </body>
+      </html>""")
+
 
   
   
@@ -153,9 +178,11 @@ def checkAuth(self):
 application = webapp.WSGIApplication([('/', MainPage),
                                       ('/errors/csv', ReportViewerCsv), 
                                       ('/errors/xml', ReportViewerXML), 
+                                      ('/usage/xml', UsageViewerXML), 
                                       ('/create_error', CreateErrorReport),
+                                      ('/create_usage', CreateUsageReport),
                                       ('/error_admin', ErrorMessagesAdmin),
-                                      ('/create_usage', CreateUsageReport) 
+                                      ('/usage_admin', UsageAdmin) 
                                       ],
                                       debug=True)
 
